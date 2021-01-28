9ac76f4c6046ef053ed39d8d8d003cf39306b406
Etienne Bonvin <etienne.bonvin@epfl.ch>
2021-01-28 17:09:49 +0100

    Creating the library, integrate travis

--- contextprinter/setup.py
+++ contextprinter/setup.py
@@ -5,12 +5,13 @@ with open("README", 'r') as f:
 
 setup(
    name='contextprinter',
-   version='1.0',
+   version='1.0.1',
    description='Tool for simple and appealing display of context in which each operation is executer',
    license="",
    long_description=long_description,
+   long_description_content_type="text/markdown",
    author='Bonvin Etienne, Rey Valérian',
    author_email='etienne.bonvin@epfl.ch, valerian.rey@epfl.ch',
    url="",
-   packages=find_packages(include=["contextprinter"])
+   packages=["contextprinter"]
 )
