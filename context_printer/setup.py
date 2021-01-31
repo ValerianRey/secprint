a6f5913fba24f936c5820cce645aad8036cffec8
EtienneBonvin <etienne.bonvin@hotmail.com>
2021-01-31 11:57:58 +0100

    Renamed README file, updated version. Version uploaded to Pypi.

--- context_printer/setup.py
+++ context_printer/setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
-with open("README", 'r') as f:
+with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
    name='context_printer',
-   version='1.0.3',
+   version='1.0.5',
    description='Tool for simple and appealing display of context in which each operation is executer',
    license="",
    long_description=long_description,
    long_description_content_type="text/markdown",
-   author='Bonvin Etienne, Rey Valérian',
-   author_email='etienne.bonvin@epfl.ch, valerian.rey@epfl.ch',
+   author="Bonvin Etienne, Rey Valérian",
+   author_email="etienne.bonvin@epfl.ch, valerian.rey@epfl.ch",
    url="",
    package_dir = {"context_printer": "context_printer"},
    packages=["context_printer"],
